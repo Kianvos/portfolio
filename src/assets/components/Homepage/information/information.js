5c5ecb6317dc0194ac05bc549055e1d98ba6f528
Kian Vos <92936961+Kianvos@users.noreply.github.com>
2023-04-28 19:50:37 +0200

    added .env.example

--- src/assets/components/Homepage/information/information.js
+++ src/assets/components/Homepage/information/information.js
@@ -4,7 +4,7 @@ const Information = () => {
     return (
         <div className={"sections"} id="information">
             <div className={"center"}>
-                <h1>Kian Vos</h1>
+                <h1 className={"name-center"}>Kian Vos</h1>
                 <h4>SOFTWARE DEVELOPER</h4>
             </div>
             <div className={"arrow"}>
