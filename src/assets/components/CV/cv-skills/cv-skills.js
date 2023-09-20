94d6d05bf2c2cd4953b7d13f82f7e4a5fd56ca3b
Kian Vos <92936961+Kianvos@users.noreply.github.com>
2023-09-20 17:09:17 +0200

    Skills geupdate cv

--- src/assets/components/CV/cv-skills/cv-skills.js
+++ src/assets/components/CV/cv-skills/cv-skills.js
@@ -12,8 +12,56 @@ const CvSkills = () => {
                     <p>Informatie</p>
                 </div>
             </Link>
-            <div className="centered-content">
-                <h1>Skills</h1>
+            <div className="centered-content languages">
+                <div className={"skills-left"}>
+                    <h2>Programmeertalen</h2>
+                    <div className="skill-bar">
+                        <div className="skill-label">Golang:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '80%' }}></div>
+                        </div>
+                    </div>
+                    <div className="skill-bar">
+                        <div className="skill-label">React:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '70%' }}></div>
+                        </div>
+                    </div>
+                    <div className="skill-bar">
+                        <div className="skill-label">Python:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '70%' }}></div>
+                        </div>
+                    </div>
+                    <div className="skill-bar">
+                        <div className="skill-label">Php:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '60%' }}></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"skills-right"}>
+                    <h2>Talen</h2>
+                    <div className="skill-bar">
+                        <div className="skill-label">Nederlands:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '100%' }}></div>
+                        </div>
+                    </div>
+                    <div className="skill-bar">
+                        <div className="skill-label">Engels:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '70%' }}></div>
+                        </div>
+                    </div>
+                    <div className="skill-bar">
+                        <div className="skill-label">Duits:</div>
+                        <div className="skill-progress">
+                            <div className="skill-progress-bar" style={{ width: '50%' }}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Link ignoreCancelEvents href={"#cv-work-experience"} to="cv-work-experience" spy={true} smooth={true} offset={-80}
                   duration={800} className={"cv-navigation-link"}>
